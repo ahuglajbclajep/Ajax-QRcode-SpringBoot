f45b6aba3b9912d7720c9352c939543c7a95d5dc
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-06-14 22:09:48 +0900

    upd use ResponseEntity<T>

--- src/main/java/ahuglajbclajep/spring/boot/ajaxqrcode/AjaxQrcodeApplication.java
+++ src/main/java/ahuglajbclajep/spring/boot/ajaxqrcode/AjaxQrcodeApplication.java
@@ -6,15 +6,16 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.EnumMap;
 import java.util.Objects;
 
@@ -23,15 +24,16 @@ import java.util.Objects;
 public class AjaxQrcodeApplication {
 
 	@PostMapping("/ajax")
-	public void hello(@RequestBody String body, HttpServletResponse res) {
-		try (OutputStream out = res.getOutputStream()) {
-			ImageIO.write(create(body), "jpg", out);
+	public ResponseEntity<byte[]> getQRcode(@RequestBody String body) {
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			ImageIO.write(createQRcode(body), "jpg", baos);
+			return ResponseEntity.ok(baos.toByteArray());
 		} catch (IOException | WriterException e) {
-			res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 	}
 
-	private BufferedImage create(String contents) throws WriterException {
+	private BufferedImage createQRcode(String contents) throws WriterException {
 		// The width and height of the image are determined by first creating an image,
 		// adding a margin (4*2 pixels), and comparing it with the specified length.
 		final int SIZE = 200;
